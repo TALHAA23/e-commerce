a0543e977ba0635210bf80915f3a068709446e4f
Talha <talhasifat23@gmail.com>
2024-04-01 10:13:40 +0500

    finalized pagination

--- src/components/ProductListing/ProductListing.jsx
+++ src/components/ProductListing/ProductListing.jsx
@@ -1,35 +1,32 @@
-import { useSearchParams } from "react-router-dom";
 import ProductCard from "./ProductCard";
 
-export default function ProductListing({ products }) {
-  const [searchParam, setSearchParam] = useSearchParams(0);
-
+export default function ProductListing({
+  pageUpdater,
+  products,
+  disablePagination = false,
+  page,
+}) {
   function changePage(diraction) {
-    const currentPage = +searchParam.get("p");
-    if (currentPage <= 2 && diraction.toLocaleLowerCase() == "prev") {
-      setSearchParam(new URLSearchParams());
-      return;
-    }
-    const updatedPage =
-      diraction.toLocaleLowerCase() == "next"
-        ? currentPage
-          ? currentPage + 1
-          : 2
-        : currentPage - 1;
-    setSearchParam(new URLSearchParams(`p=${updatedPage}`));
+    diraction = diraction.toLocaleLowerCase();
+    pageUpdater((prevPage) => {
+      const nextPage =
+        diraction == "next" ? prevPage + 1 : prevPage <= 0 ? 0 : prevPage - 1;
+      return nextPage;
+    });
   }
   return (
-    <section>
+    <section className=" relative">
       <div className="p-2 sm:p-6 flex flex-col gap-2 sm:gap-6 sm:grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
         {products.map((product) => (
           <ProductCard title={product.title} />
         ))}
       </div>
-      <div className="flex justify-center gap-3 py-3">
+      <div className=" sticky bottom-0 w-full bg-[#3c1884ea] flex justify-center gap-3 px-2 py-3 my-1">
         {["Prev", "Next"].map((btnText) => (
           <button
             onClick={() => changePage(btnText)}
-            className=" border-2 border-[#3C1884] px-5 py-1 rounded-full text-lg hover:bg-[#3C1884] hover:text-white transition-all duration-100"
+            disabled={btnText == "Prev" && page == 0 ? true : disablePagination}
+            className=" border-2 border-white text-white w-1/2 max-w-[250px] py-1 rounded-full text-lg disabled:opacity-60 hover:bg-white hover:text-black h transition-all duration-100"
           >
             {btnText}
           </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,33 @@ import getProducts from "../utils/getProducts";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 export default function Home() {
-  const [searchParam, setSearchParam] = useSearchParams(0);
+  const [page, setPage] = useState(0);
   const [lastDoc, setLastDoc] = useState(null);
-  const { isPending, isError, error, isSuccess, data } = useQuery({
-    queryKey: [`products-for-page-${searchParam.get("p")}`],
+  const {
+    isPending,
+    isError,
+    error,
+    isSuccess,
+    data,
+    isPlaceholderData,
+    isFetching,
+  } = useQuery({
+    queryKey: [`products-for-page-${page}`],
     queryFn: () => getProducts(lastDoc, setLastDoc),
     placeholderData: keepPreviousData,
-    staleTime: 60 * 60 * 60,
+    staleTime: 86400000, //24hrs
   });
 
-  if (isPending) return <h1>loading</h1>;
+  if (isPending && !isPlaceholderData) return <h1>loading</h1>;
   else if (isError) return <h1>{error.message}</h1>;
   return (
     <div>
-      <ProductListing products={data} />
+      <ProductListing
+        pageUpdater={setPage}
+        products={data}
+        disablePagination={isFetching}
+        page={page}
+      />
     </div>
   );
 }
